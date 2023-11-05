e547214a75b3188dfa03767142bad8d420d6e105
Beduman <behunz77@gmail.com>
2023-11-04 23:03:07 -0700

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string returnname()
+        {
+            Console.WriteLine("What is your name?: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int returnnumber()
+        {
+            Console.WriteLine("Choose a number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+
+
+        DisplayMessage();
+        string name = returnname();
+        int number = returnnumber();
+        Console.WriteLine($"Your name is {name}, and your number is {number}");
     }
 }
