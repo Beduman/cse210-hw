4c5b0a28516c6db0359975da664b6a8eb26b7cad
Beduman <behunz77@gmail.com>
2023-11-04 20:01:20 -0700

    Prep1 complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {firstName} {lastName}");
     }
 }
