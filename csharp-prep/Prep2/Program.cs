ad9b9de79e7f12a5e52ec2aafbee021318dcd274
Beduman <behunz77@gmail.com>
2023-11-04 20:22:30 -0700

    prep 2 and 3 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Enter your frade percentage: ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade >= 50)
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed and got a {letter}!");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, you got a {letter} and didn't pass the class.");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int Number = randomGenerator.Next(1, 100);
+        int guess = -10;
+
+
+        while (guess != Number)
+        {
+            Console.WriteLine("Guess a number between 1 and 100: ");
+            string guessword = Console.ReadLine();
+            guess = int.Parse(guessword);
+
+            if (Number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (Number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed it!");
+            }
+        }
     }
 }
