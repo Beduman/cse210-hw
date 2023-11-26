1afb0a915b1bc2e3f666552eb38eb08faaaea606
Beduman <behunz77@gmail.com>
2023-11-25 22:34:00 -0700

    BreathingActivity complete

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class BreathingActivity : Activity
 {
 
@@ -6,8 +8,43 @@ public class BreathingActivity : Activity
     {
 
     }
-    void Run()
+    public void Run()
     {
+        Console.WriteLine(_name + ": " + _description);
+        Console.WriteLine("How long would you like to do this exercise in seconds?");
+        _duration = int.Parse(Console.ReadLine());
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(_duration);
+
+        while(futureTime >= startTime)
+        {
+            Console.WriteLine("Breath in");
+            //animation
+            Thread.Sleep(1000);
+            Console.Write("4");
+            Thread.Sleep(1000);
+            Console.Write("3");
+            Thread.Sleep(1000);
+            Console.Write("2");
+            Thread.Sleep(1000);
+            Console.WriteLine("1");
+            Thread.Sleep(1000);
+            Console.WriteLine("Breath out");
+            //animation
+            Thread.Sleep(1000);
+            Console.Write("5");
+            Thread.Sleep(1000);
+            Console.Write("4");
+            Thread.Sleep(1000);
+            Console.Write("3");
+            Thread.Sleep(1000);
+            Console.Write("2");
+            Thread.Sleep(1000);
+            Console.WriteLine ("1");
+            Thread.Sleep(1000);
+
+            startTime = DateTime.Now;
+        }
 
     }
 
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,13 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        BreathingActivity breathing1 = new BreathingActivity();
         string userInput = " ";
         Console.WriteLine("Welcome to the activity program!");
         Console.WriteLine("In this program you have the option to choose a "+
                         "few activities to help calm you and keep you organized!");
-        while(userInput != "quit")
+        while(userInput != "4")
         {
-            Console.WriteLine("Enter 1 for the breathing activity");
+            Console.WriteLine("Enter '1' for the breathing activity");
+            Console.WriteLine("Enter '4' to exit the program");
+            userInput = Console.ReadLine();
+            if(userInput == "1")
+            {
+                breathing1.Run();
+                userInput = " ";
+            }
         }
     }
 }
